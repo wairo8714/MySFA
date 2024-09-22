0aa4ef164b9f030e706f4c84e9e000f8aa449255
wairo <d-m.navigate@outlook.jp>
2024-09-22 22:03:41 +0900

    Temporarily disable collectstatic

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
