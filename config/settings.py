b907829ad2a6e43d72e01c000301a8a115bbbe95
wairo <d-m.navigate@outlook.jp>
2024-09-23 22:14:19 +0900

    Update S3 bucket policy and media file settings

--- config/settings.py
+++ config/settings.py
@@ -169,8 +169,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 # メディアファイルの設定
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-MEDIAFILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # 静的ファイルの設定
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
