cde2578a4a1a8cf07737c42a393e61a0a1122c0a
wairo <d-m.navigate@outlook.jp>
2024-09-24 21:58:24 +0900

    whitenoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,7 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
