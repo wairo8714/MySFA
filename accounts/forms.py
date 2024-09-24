31c0249915f2c75f186f7a44458ad80c32c2be17
wairo <d-m.navigate@outlook.jp>
2024-09-24 21:49:17 +0900

    TwitterCard.png

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    custom_user_id = forms.CharField(max_length=30, required=True)
+    
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'custom_user_id', 'password1', 'password2', 'question', 'answer',)
