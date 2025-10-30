9ecaa32258c2446d81f3a408a64424edbe5c99d5
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-10-30 12:01:20 +0900

    fix: S3再設定

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -16,68 +16,58 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-# SSHアクセス許可（例示不要、必要に応じて上書き）
 variable "allowed_ssh_cidrs" {
   description = "CIDR blocks allowed to SSH access"
   type        = list(string)
   default     = ["0.0.0.0/0"]
 }
 
-# アプリケーション設定
 variable "dockerhub_username" {
   description = "Docker Hub username for image pulling"
   type        = string
-  default     = ""
 }
 
-variable "domain_name" {
-  description = "Domain name for the application"
-  type        = string
-  default     = "mysfa.net"
-}
-
-# DB / Django 設定（環境変数で注入）
 variable "mysql_host" {
   description = "MySQL host"
   type        = string
-  default     = ""
 }
 
 variable "mysql_database" {
   description = "MySQL database name"
   type        = string
-  default     = ""
+  default     = "mysfa_db"
 }
 
 variable "mysql_user" {
   description = "MySQL user"
   type        = string
-  default     = ""
+  default     = "admin"
 }
 
 variable "mysql_password" {
   description = "MySQL password"
   type        = string
   sensitive   = true
-  default     = ""
 }
 
 variable "mysql_root_password" {
   description = "MySQL root password"
   type        = string
   sensitive   = true
-  default     = ""
 }
 
 variable "secret_key" {
   description = "Django secret key"
   type        = string
   sensitive   = true
-  default     = ""
 }
 
 variable "allowed_hosts" {
   description = "Django ALLOWED_HOSTS setting"
   type        = string
-  default     = "*"
+}
+
+variable "domain_name" {
+  description = "Domain name for the application"
+  type        = string
 }
