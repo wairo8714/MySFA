5a027d1be104f81144a1ca3f1550ad97370d3020
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-11-04 13:26:15 +0900

    fix: インスタンスタイプ変更(t3.micro→small)

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -13,7 +13,7 @@ variable "project_name" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.small"
 }
 
 variable "allowed_ssh_cidrs" {
