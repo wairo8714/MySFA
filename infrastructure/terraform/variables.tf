34545c02e660479b5e8bea9decacda7629f1d1a6
wairo <d-m.navigate@outlook.jp>
2025-11-04 23:50:25 +0900

    fix: テスト

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -74,9 +74,3 @@ variable "s3_bucket_name" {
   description = "S3 bucket name for static and media files"
   type        = string
 }
-
-variable "public_key" {
-  description = "SSH public key for EC2 instance"
-  type        = string
-  sensitive   = true
-}
