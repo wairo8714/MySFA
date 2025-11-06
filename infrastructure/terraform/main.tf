bd87303f8cc7707802b68f5a9afe73b5d1e4cf87
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-11-06 15:43:50 +0900

    fix: ターゲットグループの明示

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -153,6 +153,7 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
+  target_type = "ip"
 
   health_check {
     enabled             = true
