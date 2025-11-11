399c359605efced27d9ebb52ca6f14cc9c3f55b1
wairo <d-m.navigate@outlook.jp>
2025-11-12 00:19:19 +0900

    fix:修正

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ set -e
 # Gunicorn をバックグラウンドで起動
 poetry run gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --workers 3 &
+    --workers 3 \
+    --access-logfile - \
+    --error-logfile - &
 
 # Nginx をフォアグラウンドで起動
 nginx -g "daemon off;"
