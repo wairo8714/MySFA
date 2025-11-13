14fa472b43cec3fbba71e387ddc0472c2a1ac7d3
wairo <d-m.navigate@outlook.jp>
2025-11-14 04:37:56 +0900

    fix:404エラー修正

--- src/config/settings.py
+++ src/config/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "config.middleware.HealthCheckMiddleware",  # ALBのHTTPヘルスチェック用
 ]
 
 ROOT_URLCONF = "config.urls"
