0b3618fa36c9b0439590352492ba24546937f6c5
wairo <d-m.navigate@outlook.jp>
2025-11-16 22:21:47 +0900

    fix:

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -355,7 +355,7 @@ resource "aws_ecs_task_definition" "app" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
-  memory                   = "1024"
+  memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
 
