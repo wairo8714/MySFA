e2354f3a9e74d9cee1693fa11b2ad907cac6af65
wairo <d-m.navigate@outlook.jp>
2025-11-16 16:50:11 +0900

    fix: Aレコード追加

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -165,6 +165,17 @@ resource "aws_route53_record" "main" {
   }
 }
 
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
 # ============================================
 # S3 / ECR / IAM / ECS
 # ============================================
