ef453d4d4aae385da4861a397420ffe1536721c2
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-11-20 14:46:58 +0900

    feature: S3モジュールの集約
    Removed unused outputs and added VPC and S3 bucket outputs.

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,39 +1,32 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.main.id
+# VPC 情報
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
 }
 
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
+
+# ALB / Route53
 output "alb_dns_name" {
   description = "DNS name of the Application Load Balancer"
   value       = aws_lb.main.dns_name
 }
 
-output "alb_arn" {
-  description = "ARN of the Application Load Balancer"
-  value       = aws_lb.main.arn
-}
-
-output "target_group_arn" {
-  description = "ARN of the target group"
-  value       = aws_lb_target_group.main.arn
-}
-
-output "acm_certificate_arn" {
-  description = "ARN of the ACM certificate"
-  value       = aws_acm_certificate.main.arn
-}
-
 output "application_url_https" {
-  description = "HTTPS URL to access the application (via ALB)"
+  description = "HTTPS URL to access the application"
   value       = "https://${var.domain_name}"
 }
 
-output "application_url_http" {
-  description = "HTTP URL to access the application (redirects to HTTPS via ALB)"
-  value       = "http://${var.domain_name}"
+# S3（static / media）
+output "s3_app_bucket_name" {
+  description = "S3 bucket name for static and media files"
+  value       = module.s3_app.bucket_name
 }
 
-output "ami_id" {
-  description = "AMI ID used for the instance"
-  value       = data.aws_ami.amazon_linux.id
+output "s3_app_bucket_arn" {
+  description = "S3 bucket ARN for static and media files"
+  value       = module.s3_app.bucket_arn
 }
