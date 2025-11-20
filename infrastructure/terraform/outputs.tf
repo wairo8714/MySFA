01991ed3454cbaabf99c5b4855d463f5aadb8a38
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-11-20 15:46:14 +0900

    feature: IAMロールモジュールの集約

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -37,3 +37,13 @@ output "ecr_app_repository_url" {
   value       = module.ecr_app.repository_url
 }
 
+#IAM
+output "ecs_task_execution_role_arn" {
+  description = "ARN of ECS task execution role"
+  value       = module.ecs_iam.task_execution_role_arn
+}
+
+output "ecs_task_role_arn" {
+  description = "ARN of ECS task role"
+  value       = module.ecs_iam.task_role_arn
+}
