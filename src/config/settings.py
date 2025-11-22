da9f4046ab8cfd537743d9e7cdb657456557a602
wairo <d-m.navigate@outlook.jp>
2025-11-22 14:41:13 +0900

    fix: ACL書き込み許可

--- src/config/settings.py
+++ src/config/settings.py
@@ -100,7 +100,6 @@ if USE_S3:
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = os.getenv("AWS_REGION", "ap-northeast-1")
     AWS_QUERYSTRING_AUTH = False
-    AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = (
         f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
