24d30f14af29958c3c7a3a5fb743f0d804425ad9
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-11-28 13:54:27 +0900

    feature: Private subnet作成

--- infrastructure/terraform/modules/vpc/outputs.tf
+++ infrastructure/terraform/modules/vpc/outputs.tf
@@ -14,3 +14,18 @@ output "public_route_table_id" {
   description = "ID of the public route table"
   value       = aws_route_table.public.id
 }
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table"
+  value       = aws_route_table.private.id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.main.id
+}
