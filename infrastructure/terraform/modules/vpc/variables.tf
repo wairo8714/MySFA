39ecc3cc9387b9df52af9bcc424ba25c29d80248
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-11-28 13:11:48 +0900

    feature: Private subnet作成

--- infrastructure/terraform/modules/vpc/variables.tf
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -13,3 +13,7 @@ variable "public_subnet_cidrs" {
   type        = list(string)
 }
 
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
