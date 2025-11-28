25b62cc47e48adda64765ee39a81ee86765daa49
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-11-28 11:58:24 +0900

    feature: private subnetの作成

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -28,9 +28,10 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
 
-  project_name        = var.project_name
-  vpc_cidr            = var.vpc_cidr
-  public_subnet_cidrs = var.public_subnet_cidrs
+  project_name         = var.project_name
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
 
 # ============================================
