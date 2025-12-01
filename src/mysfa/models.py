db7ab97a14bc2355bc5f0a241e89aade98d16882
wairo <d-m.navigate@outlook.jp>
2025-12-01 23:45:37 +0900

    fix: 不要テーブル削除

--- src/mysfa/models.py
+++ src/mysfa/models.py
@@ -7,13 +7,6 @@ from django.contrib.auth.models import Group as AuthGroup
 from django.db import models
 
 
-class MyModel(models.Model):
-    file = models.FileField(upload_to="uploads/")
-
-    def __str__(self):
-        return self.file.name
-
-
 class Post(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, verbose_name="投稿者", on_delete=models.CASCADE
