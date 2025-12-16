68c8f2d5f991cefe0a8b3ea72a2bec51c3720204
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-12-16 10:14:52 +0900

    feature: demoモード実装

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     CheckUserIdView,
     CustomLogoutView,
+    DemoLoginView,
     DeleteAccountView,
     ForgotPasswordView,
     PasswordResetView,
@@ -13,6 +14,7 @@ from .views import (
 app_name = "accounts"
 
 urlpatterns = [
+    path("demo/", DemoLoginView.as_view(), name=demo_login"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("forgot_password/", ForgotPasswordView.as_view(), name="forgot_password"),
     path("verify_answer/", VerifyAnswerView.as_view(), name="verify_answer"),
