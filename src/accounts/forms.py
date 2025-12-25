c1e677a690278fcd3af933b83ce189f2b77ac220
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-12-25 14:09:29 +0900

    fix: forms内max_length再定義

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -4,9 +4,60 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserCreationForm(UserCreationForm):
-    custom_user_id = forms.CharField(max_length=15, required=True)
-    question = forms.CharField(max_length=20, required=True)
-    answer = forms.CharField(max_length=20, required=True)
+    custom_user_id = forms.CharField(
+        label="ID",
+        max_length=15,
+        required=True,
+    )
+    
+    question = forms.CharField(
+        label="質問",
+        max_length=20,
+        required=True,
+    )
+    
+    answer = forms.CharField(
+        label="質問の答え",
+        max_length=20, 
+        required=True,
+    )
+
+    password1 = forms.CharField(
+        label="パスワード",
+        strip=False,
+        max_length=20,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                #min_lengthはsettings.pyにて定義済
+                "maxlength": "20",
+            }
+        ),
+        help_text="5〜20文字で入力してください。",
+        error_messages={
+            "max_length": "パスワードは20文字以内で入力してください。",
+            "required": "パスワードを入力してください。",
+        },
+    )
+
+    password2 = forms.CharField(
+        label="Password confirmation",
+        strip=False,
+        max_length=20,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "maxlength": "20",
+            }
+        ),
+        help_text="確認のため、同じパスワードをもう一度入力してください。",
+        error_messages={
+            "max_length": "パスワード（確認）は20文字以内で入力してください。",
+            "required": "確認用パスワードを入力してください。",
+        },
+    )
 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
