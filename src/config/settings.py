bc93256fe85b837752d48780ce817b22f68d861a
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2025-12-26 15:05:33 +0900

    fix: typo修正

--- src/config/settings.py
+++ src/config/settings.py
@@ -87,7 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-        "OPTIONs": {"min_length": 5},
+        "OPTIONS": {"min_length": 5},
     },
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
