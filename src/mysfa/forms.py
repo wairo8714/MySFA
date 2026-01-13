6b18c5536701736d87ff3db89566114c3e6e14ce
wairo8714 <120913002+wairo8714@users.noreply.github.com>
2026-01-13 13:31:42 +0900

    chore: ProductMasterFormの追加

--- src/mysfa/forms.py
+++ src/mysfa/forms.py
@@ -2,13 +2,34 @@ from django import forms
 
 from accounts.models import CustomUser
 
-from .models import Group, Post
+from .models import Group, Post, ProductMaster
 
 
 class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = ["name"]
+
+class ProductMasterForm(forms.ModelForm):
+    class Meta:
+        model = ProductMaster
+        fields = [
+            "product_code",
+            "name",
+            "category_main",
+            "category_sub",
+            "cost_price",
+            "price_excl_tax",
+            "price_incl_tax",
+            "custom_text_1",
+            "custom_text_2",
+            "custom_text_3",
+            "custom_int_1",
+            "custom_int_2",
+            "custom_int_3",
+            "custom_date_1",
+            "description",
+        ]
 
 
 class PostForm(forms.ModelForm):
